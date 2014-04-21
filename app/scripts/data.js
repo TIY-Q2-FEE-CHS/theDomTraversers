783331675d92944b8ae66980988b8a6552874845
Katherine Mattison <kbmedisto@gmail.com>
2014-04-21 16:17:30 -0400

    commit new data js

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -7,7 +7,13 @@ var location1 = new Location("east coast", 5);
 var location2 = new Location("mid west", 10);
 
 
-var ship1 = new ship("next day", 20);
-var ship2 = new ship("ground", 5);
+var ship1 = new Ship("next day", 20);
+var ship2 = new Ship("ground", 5);
+
+$(document).ready(function) {
+
+var 
+var 
 
 
+}

b676e9892b9c787d8dd3bdf66ae6518a6a2cfe12
Andrew Raynal <andrewraynal@Andrews-MacBook-Air.local>
2014-04-22 16:08:46 -0400

    about info

--- app/scripts/about.js
+++ app/scripts/about.js
@@ -1,10 +1,10 @@
 
 var about = [
 {//AndrewInfo
-	"avatar_url":
-	"title": "Partner"
-	"hometown":
-	"hobby":
+	"avatar_url": "https://avatars1.githubusercontent.com/u/7121310?s=140",
+	"title": "Partner",
+	"hometown": "Clinton",
+	"hobby": "github",
 },
 {//KatInfo
 	"avatar_url":

342e696e490f740b99e7b1d079fd5b9bd65f8540
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-04-22 16:16:48 -0400

    shawn about info

--- app/scripts/about.js
+++ app/scripts/about.js
@@ -13,10 +13,10 @@ var about = [
 	"hobby":
 },
 {//ShawnInfo
-	"avatar_url":
-	"title": "Marketing Manager"
-	"hometown":
-	"hobby":
+	"avatar_url": "https://avatars2.githubusercontent.com/u/7126379?u=b0bf7eaa2bd056343be9b148e2571d9b351dc344&s=140",
+	"title": "Marketing Manager",
+	"hometown": "Georgetown, SC",
+	"hobby": "Picking things up and putting things down"
 }
 ];
 

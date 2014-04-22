148d0893c9e03eddba15d3f28f4fa29ae9a3b888
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-04-22 12:58:40 -0400

    fixed spacing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,13 +7,11 @@ function Monitor(size, cost) {
 function Ship(time, cost) {
 	this.time = time;
 	this.cost = cost;
-
 }
 
 function Location(area, cost) {
 	this.area = area;
 	this.cost = cost;
-
 }
 			
 

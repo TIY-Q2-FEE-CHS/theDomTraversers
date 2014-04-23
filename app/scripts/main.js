18fdb021b1eb0529fd1a7351c565e737ae5dc527
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-04-23 17:28:53 -0400

    more edits

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,32 +1,57 @@
-
-// function Monitor(size, cost) {
-// 	this.size = size;
-// 	this.cost = cost;
-// }
-
-// function Ship(time, cost) {
-// 	this.time = time;
-// 	this.cost = cost;
-// }
-
-// function Location(area, cost) {
-// 	this.area = area;
-// 	this.cost = cost;
-// }
-
-
-
-// finally, this comment is better
-// this is a comment
-
-// for product gallery on shop page			
 $(document).ready(function() {
 
+
+// Product gallery with description
 var galleryTemplate = $('#gallery').html();
 var galleryOptions = _.template(galleryTemplate, productType);
 $('.jumbotron').append(galleryOptions);
 
+
+
+// Adding customer options together
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
